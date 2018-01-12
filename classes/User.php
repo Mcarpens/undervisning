4aefa50f7ede911fae6096f159418be0c205e8ec
Mcarpens <Mcarpens@hotmail.com>
2018-01-12 10:42:53 +0100

    Rev 11

--- classes/User.php
+++ classes/User.php
@@ -33,11 +33,6 @@ class User extends \PDO
         $this->db = $db;
     }
 
-    public function checkUsername($username) {
-        return $this->db->single("SELECT username FROM users WHERE username=:uname",
-                                [':uname' => $username]);
-    }
-
     # Only accept the $_POST inputs and not $_GET inputs.
 
     /**
@@ -60,119 +55,6 @@ class User extends \PDO
      */
     public function secGetInputArray($input) {
         return filter_input_array(strtoupper($input), FILTER_SANITIZE_SPECIAL_CHARS);
-    }
-
-    ## TJEK OM BRUGER EKSISTERE, return true, hvis ja
-
-    /**
-     * @param $username
-     * @return bool
-     */
-    private function userExists($username) {
-        $this->username = $username;
-        if ($stmt = $this->db->prepare("SELECT id FROM users WHERE username = :username")) {
-            $stmt->bindParam(':username', $this->username);
-            if ($stmt->execute()) {
-                if ($stmt->rowCount() > 0) {
-                    return true;
-                } else {
-                    return false;
-                }
-            } else {
-                return true;
-            }
-        } else {
-            return true;
-        }
-    }
-
-    ## Opret ny bruger
-
-    /**
-     * @param $username
-     * @param $password
-     * @return bool
-     */
-    public function create($username, $password) {
-        $this->username = $username;
-        $this->password = $password;
-
-        if (!$this->userExists($this->username)) {
-            // Connect to database
-            if ($stmt = $this->db->prepare("INSERT INTO users (username, password, fk_userrole) VALUES (:username, :password, '3')")) {
-                $stmt->bindParam(':username', $this->username);
-                $stmt->bindParam(':password', password_hash($this->password, PASSWORD_BCRYPT, ['cost' => 12]));
-                if ($stmt->execute()) {
-                    return true;
-                } else {
-                    return false;
-                }
-            } else {
-                return false;
-            }
-        } else {
-            return false;
-        }
-    }
-
-    ## Get the latest userID
-
-    /**
-     * @return mixed
-     */
-    public function getLastUserId() {
-        $result = $this->db->query("SELECT id FROM users ORDER BY id DESC LIMIT 1");
-        $row = $result->fetch(PDO::FETCH_ASSOC);
-        return $row['id'];
-    }
-
-    ## Verify users
-
-    /**
-     * @param $username
-     * @param $password
-     * @return bool
-     */
-    public function verifyUser($username, $password) {
-        $this->username = $username;
-        $this->password = $password;
-
-        if ($stmt = $this->db->prepare("SELECT id, password FROM users WHERE username = :username")) {
-            $stmt->bindParam(':username', $this->username, PDO::PARAM_STR);
-            if ($stmt->execute()) {
-                $result = $stmt->fetch(PDO::FETCH_ASSOC);
-                if (password_verify($this->password, $result['password'])) {
-                    return true;
-                } else {
-                    return false;
-                }
-            } else {
-                return false;
-            }
-        } else {
-            return false;
-        }
-    }
-
-    ## Get userID from users
-
-    /**
-     * @param $username
-     * @return bool
-     */
-    public function getUserId($username) {
-        $this->username = $username;
-        if ($stmt = $this->db->prepare("SELECT id FROM users WHERE username = :username")) {
-            $stmt->bindParam(':username', $this->username, PDO::PARAM_STR);
-            if ($stmt->execute()){
-                $result = $stmt->fetch(PDO::FETCH_ASSOC);
-                return $result['id'];
-            } else {
-                return false;
-            }
-        } else {
-            return false;
-        }
     }
 
     ## Login Method Filter Function
@@ -307,7 +189,7 @@ class User extends \PDO
         $stmt = $this->db->prepare("SELECT userroles.niveau FROM `users`
 								INNER JOIN `userroles` ON `userroles`.`id` = `users`.`fk_userrole`
 								WHERE `users`.`id` = :id");
-        $stmt->bindParam(':id', $_SESSION['user_session'], PDO::PARAM_STR);
+        $stmt->bindParam(':id', $_SESSION['user_id'], PDO::PARAM_STR);
         $stmt->execute();
         if($stmt->rowCount() === 1){
             $result = $stmt->fetch(PDO::FETCH_OBJ);
@@ -318,42 +200,12 @@ class User extends \PDO
     }
 
     /**
-     * @param $id
+     * @param $username
      * @return mixed
      */
-    public function userGet($id) {
-        $stmt = $this->db->prepare("SELECT userroles.niveau, users.username, users.firstname, users.lastname, users.id
-                             FROM users 
-                             INNER JOIN userroles
-                             ON users.fk_userrole = userroles.id
-                             WHERE users.id = :id");
-        $stmt->bindParam(':id', $id, PDO::PARAM_INT);
-        if($stmt->execute() && ($stmt->rowCount() === 1)) {
-            return $stmt->fetch(PDO::FETCH_ASSOC);
-        }
-    }
-
-    /**
-     * @param $sql
-     * @param $id
-     * @return mixed
-     */
-    public function getFromDB($sql, $id) {
-        $stmt = $this->db->prepare($sql);
-        $stmt->bindParam(':id', $id, PDO::PARAM_INT);
-        if($stmt->execute() && ($stmt->rowCount() === 1)) {
-            return $stmt->fetch(PDO::FETCH_ASSOC);
-        }
-    }
-
-    /**
-     * @param $sql
-     * @return mixed
-     */
-    public function runQuery($sql)
-    {
-        $stmt = $this->db->prepare($sql);
-        return $stmt;
+    public function checkUsername($username) {
+        return $this->db->single("SELECT username FROM users WHERE username=:uname",
+            [':uname' => $username]);
     }
 
     /**
