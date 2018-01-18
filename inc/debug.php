f1edd5aca18107150a923f9f62ccb8523f34c1bb
Mcarpens <Mcarpens@hotmail.com>
2018-01-18 09:39:37 +0100

    Rev 26

--- inc/debug.php
+++ inc/debug.php
@@ -6,28 +6,12 @@
  * Time: 13:01
  */
 
-if(isset($_POST['hide'])) {
-    setcookie("HideDebugMenu",
-        $_COOKIE['HideDebugMenu'] = "Hidden", time()+60*60*24*5, "/");
-}
-if(isset($_COOKIE['HideDebugMenu'])) {
-    var_dump($_COOKIE);
-} else {
-    echo 'NOPE!';
-}
 ?>
-<div class="usrp-fb-2 is-expanded">
-<?php
-if(isset($_COOKIE['HideDebugMenu'])) {
-
-    ?>
+<div class="usrp-fb-2 slide-in">
     <div class="usrp-fb-state-collapsed">
         <div class="usrp-fb-title"> &nbsp; DEBUG &nbsp;</div>
     </div>
 
-    <?php
-} else {
-    ?>'
     <div class="usrp-fb-state-expanded">
         <div class="usrp-fb-title"><i class="fa fa-bug"></i> DEBUG MENU <i class="fa fa-bug"></i></div>
         <?php print_r($_SESSION) ?>
@@ -36,11 +20,4 @@ if(isset($_COOKIE['HideDebugMenu'])) {
             <button type="submit" class="usrp-fb-btn usrp-fb-btn-yes" name="hide">SKJUL</button>
         </form>
     </div>
-
-
-    <?php
-}
-?>
-
-
 </div>
